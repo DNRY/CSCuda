6cde8393939ba8296799d1fc122f86d9b8590f83
HanByul Yang <hanbyul.yang@gmail.com>
2016-09-22 08:42:26 +0900

    added assembly info

--- CSCuda.NPP/Properties/AssemblyInfo.cs
+++ CSCuda.NPP/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CSCuda.NPP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# wrapper for CUDA 7.5.18")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DNRY")]
 [assembly: AssemblyProduct("CSCuda.NPP")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
