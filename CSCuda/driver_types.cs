abd8c94099ee220074497383255ea1457d3b60e5
HanByul Yang <hanbyul.yang@gmail.com>
2016-09-21 15:19:53 +0900

    added some essential cuda runtime api

--- CSCuda/driver_types.cs
+++ CSCuda/driver_types.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace CSCuda
@@ -605,5 +606,125 @@ namespace CSCuda
         DeviceToHost = 2,    // Device -> Host
         DeviceToDevice = 3,  // Device -> Device
         Default = 4          // Default based unified virtual address space
+    }
+
+    /// <summary>
+    /// CUDA device properties
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct cudaDeviceProp
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public sbyte[] name;                  // ASCII string identifying device
+        public Int64 totalGlobalMem;             // Global memory available on device in bytes
+        public Int64 sharedMemPerBlock;          // Shared memory available per block in bytes
+        public int regsPerBlock;               // 32-bit registers available per block
+        public int warpSize;                   // Warp size in threads
+        public Int64 memPitch;                   // Maximum pitch in bytes allowed by memory copies
+        public int maxThreadsPerBlock;         // Maximum number of threads per block
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxThreadsDim;           // Maximum size of each dimension of a block
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxGridSize;             // Maximum size of each dimension of a grid
+        public int clockRate;                  // Clock frequency in kilohertz
+        public Int64 totalConstMem;              // Constant memory available on device in bytes
+        public int major;                      // Major compute capability
+        public int minor;                      // Minor compute capability
+        public Int64 textureAlignment;           // Alignment requirement for textures
+        public Int64 texturePitchAlignment;      // Pitch alignment requirement for texture references bound to pitched memory
+        public int deviceOverlap;              // Device can concurrently copy memory and execute a kernel. Deprecated. Use instead asyncEngineCount.
+        public int multiProcessorCount;        // Number of multiprocessors on device
+        public int kernelExecTimeoutEnabled;   // Specified whether there is a run time limit on kernels
+        public int integrated;                 // Device is integrated as opposed to discrete
+        public int canMapHostMemory;           // Device can map host memory with cudaHostAlloc/cudaHostGetDevicePointer
+        public int computeMode;                // Compute mode (See ::cudaComputeMode)
+        public int maxTexture1D;               // Maximum 1D texture size
+        public int maxTexture1DMipmap;         // Maximum 1D mipmapped texture size
+        public int maxTexture1DLinear;         // Maximum size for 1D textures bound to linear memory
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxTexture2D;            // Maximum 2D texture dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxTexture2DMipmap;      // Maximum 2D mipmapped texture dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxTexture2DLinear;      // Maximum dimensions (width, height, pitch) for 2D textures bound to pitched memory
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxTexture2DGather;      // Maximum 2D texture dimensions if texture gather operations have to be performed
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxTexture3D;            // Maximum 3D texture dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxTexture3DAlt;         // Maximum alternate 3D texture dimensions
+        public int maxTextureCubemap;          // Maximum Cubemap texture dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxTexture1DLayered;     // Maximum 1D layered texture dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxTexture2DLayered;     // Maximum 2D layered texture dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxTextureCubemapLayered;// Maximum Cubemap layered texture dimensions
+        public int maxSurface1D;               // Maximum 1D surface size
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxSurface2D;            // Maximum 2D surface dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxSurface3D;            // Maximum 3D surface dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxSurface1DLayered;     // Maximum 1D layered surface dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public int[] maxSurface2DLayered;     // Maximum 2D layered surface dimensions
+        public int maxSurfaceCubemap;          // Maximum Cubemap surface dimensions
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public int[] maxSurfaceCubemapLayered;// Maximum Cubemap layered surface dimensions
+        public Int64 surfaceAlignment;           // Alignment requirements for surfaces
+        public int concurrentKernels;          // Device can possibly execute multiple kernels concurrently
+        public int ECCEnabled;                 // Device has ECC support enabled
+        public int pciBusID;                   // PCI bus ID of the device
+        public int pciDeviceID;                // PCI device ID of the device
+        public int pciDomainID;                // PCI domain ID of the device
+        public int tccDriver;                  // 1 if device is a Tesla device using TCC driver, 0 otherwise
+        public int asyncEngineCount;           // Number of asynchronous engines
+        public int unifiedAddressing;          // Device shares a unified address space with the host
+        public int memoryClockRate;            // Peak memory clock frequency in kilohertz
+        public int memoryBusWidth;             // Global memory bus width in bits
+        public int l2CacheSize;                // Size of L2 cache in bytes
+        public int maxThreadsPerMultiProcessor;// Maximum resident threads per multiprocessor
+        public int streamPrioritiesSupported;  // Device supports stream priorities
+        public int globalL1CacheSupported;     // Device supports caching globals in L1
+        public int localL1CacheSupported;      // Device supports caching locals in L1
+        public Int64 sharedMemPerMultiprocessor; // Shared memory available per multiprocessor in bytes
+        public int regsPerMultiprocessor;      // 32-bit registers available per multiprocessor
+        public int managedMemory;              // Device supports allocating managed memory on this system
+        public int isMultiGpuBoard;            // Device is on a multi-GPU board
+        public int multiGpuBoardGroupID;       // Unique identifier for a group of devices on the same multi-GPU board
+    }
+
+    /// <summary>
+    /// Channel format kind
+    /// </summary>
+    public enum cudaChannelFormatKind
+    {
+        cudaChannelFormatKindSigned = 0,      // Signed channel format
+        cudaChannelFormatKindUnsigned = 1,      // Unsigned channel format
+        cudaChannelFormatKindFloat = 2,      // Float channel format
+        cudaChannelFormatKindNone = 3       // No channel format
+    }
+
+    /// <summary>
+    /// CUDA Channel format descriptor
+    /// </summary>
+    public struct cudaChannelFormatDesc
+    {
+        public int x; // x
+        public int y; // y
+        public int z; // z
+        public int w; // w
+        public cudaChannelFormatKind f; // Channel format kind
+    }
+
+    /// <summary>
+    /// CUDA extent
+    /// </summary>
+    public struct cudaExtent
+    {
+        public long width;     // Width in elements when referring to array memory, in bytes when referring to linear memory
+        public long height;    // Height in elements
+        public long depth;     // Depth in elements
     }
 }
