093f1aa151ac3585919b6833e711f33aa139709b
HanByul Yang <hanbyul.yang@gmail.com>
2016-09-23 15:48:38 +0900

    Added CSCuda.CUBLAS project and some runtime api
    - cublasCreate()
    - cublasDestroy()
    - cublasSgemm()
    - cublasCgemm()

---
+++ CSCuda.CUBLAS/cublas_api.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCuda.CUBLAS
+{
+    public enum cublasStatus_t
+    {
+        CUBLAS_STATUS_SUCCESS = 0,
+        CUBLAS_STATUS_NOT_INITIALIZED = 1,
+        CUBLAS_STATUS_ALLOC_FAILED = 3,
+        CUBLAS_STATUS_INVALID_VALUE = 7,
+        CUBLAS_STATUS_ARCH_MISMATCH = 8,
+        CUBLAS_STATUS_MAPPING_ERROR = 11,
+        CUBLAS_STATUS_EXECUTION_FAILED = 13,
+        CUBLAS_STATUS_INTERNAL_ERROR = 14,
+        CUBLAS_STATUS_NOT_SUPPORTED = 15,
+        CUBLAS_STATUS_LICENSE_ERROR = 16
+    }
+
+    public enum cublasOperation_t
+    {
+        CUBLAS_OP_N = 0,
+        CUBLAS_OP_T = 1,
+        CUBLAS_OP_C = 2
+    }
+}
