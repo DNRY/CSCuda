8d78062a28552dd47384870846744ee446fac34a
HanByul Yang <hanbyul.yang@gmail.com>
2016-09-23 15:43:21 +0900

    Added float2 vector type

---
+++ CSCuda/vector_types.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSCuda
+{
+    public struct float2
+    {
+        public float X;
+        public float Y;
+
+        public float2(string value)
+        {
+            var values = value.Split(',');
+
+            X = Convert.ToSingle(values[0]);
+            Y = Convert.ToSingle(values[1]);
+        }
+
+        public float2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float2(double x, double y)
+        {
+            X = Convert.ToSingle(x);
+            Y = Convert.ToSingle(y);
+        }
+
+        public float2(int x, int y)
+        {
+            X = Convert.ToSingle(x);
+            Y = Convert.ToSingle(y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", X, Y);
+        }
+    }
+}
