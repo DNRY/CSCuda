bca1fa03a0a96e04430eecc901d3da18036e0dec
HanByul Yang <hanbyul.yang@gmail.com>
2018-01-26 11:17:01 +0900

    Automated Release notes and year

--- pack.ps1
+++ pack.ps1
@@ -2,10 +2,16 @@ $root = (split-path -parent $MyInvocation.MyCommand.Definition)
 $version = [System.Reflection.Assembly]::LoadFile("$root\CSCuda\bin\Release\CSCuda.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
+$commitLog = git log -1 --pretty=%B
+$copyrightYear = (Get-Date).year
+
 Write-Host "Setting .nuspec version tag to $versionStr"
 
 $content = (Get-Content $root\CSCuda.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
+$content = $content -replace '\$releaseNotes\$',$commitLog
+$content = $content -replace '\$copyrightYear\$',$copyrightYear
+
 
 $content | Out-File $root\CSCuda.compiled.nuspec
 
